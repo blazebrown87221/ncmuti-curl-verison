a6d6245a09590b3b2fc39caa1759d5b2d557484b
blazebrown87221 <96564866+blazebrown87221@users.noreply.github.com>
2023-12-31 21:16:17 -0600

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,43 @@
+@echo off
+:start
+mode 80, 28
+title ncmuti - blazebrown8722
+chcp 65001 >nul
+echo .
+echo .
+echo .
+echo .
+ping localhost -n 2 >nul
+echo              [34m███    ██  ██████ ███    ███ ██    ██ ████████ ██ [0m
+ping localhost -n 2 >nul
+echo              [34m████   ██ ██      ████  ████ ██    ██    ██    ██ [0m
+ping localhost -n 2 >nul
+echo              [96m██ ██  ██ ██      ██ ████ ██ ██    ██    ██    ██ [0m
+ping localhost -n 2 >nul
+echo              [36m██  ██ ██ ██      ██  ██  ██ ██    ██    ██    ██ [0m
+ping localhost -n 2 >nul
+echo              [36m██   ████  ██████ ██      ██  ██████     ██    ██ [0m
+echo .
+echo .
+echo .
+echo .
+:input                                          
+ping localhost -n 2 >nul
+echo ─┬───────net cat - 1 - listener - port:25565
+ping localhost -n 2 >nul
+echo  └───────────ur ip address - 2 - run ipconfig
+choice /c 12 >nul
+if "%errorlevel%" EQU "1" (
+    cd files
+	cd nc
+	echo starting to listen...
+	start nc -l -p 25565
+	pause
+	goto start
+) else if "%errorlevel%" EQU "2" (
+    cd files
+	cd yourip
+	start yourip.bat
+    pause
+    goto start
+)
